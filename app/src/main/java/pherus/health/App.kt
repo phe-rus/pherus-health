74200a223d839ce72480172bf6f796e4090e780f
la-niina <la.niina.me@gmail.com>
2024-04-27 23:05:41 +0300

    App - update add MMKV

--- app/src/main/java/pherus/health/App.kt
+++ app/src/main/java/pherus/health/App.kt
@@ -1,6 +1,11 @@
 package pherus.health
 
 import androidx.multidex.MultiDexApplication
+import com.tencent.mmkv.MMKV
 
 class App : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        MMKV.initialize(this)
+    }
 }
